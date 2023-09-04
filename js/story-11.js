db8df8f3625750c905ab835f7788285e8f581c2b
SEIMIN-BJJ <interkq12@gmail.com>
2023-09-04 23:30:33 +0900

    .

--- js/story-11.js
+++ js/story-11.js
@@ -1,20 +1,19 @@
-const content ="세상은 아직 살만하다." 
-    
-    const text = document.getElementById('text');
-    
-    let i = 0;
-    
-    function typing() {
-      
-      if( i < content.length) {
-      let txt = content.charAt(i);
-      text.innerHTML += txt === "`"?"<br/>" : txt; i++;
-      }
-    }
-    setInterval(typing, 500);
+const content = "세상은 아직 살만하다.";
 
+const text = document.getElementById("text");
 
-text.addEventListener('click', function () {
-  let page = 'https://seimin123.github.io/VER-1/';
+let i = 0;
+
+function typing() {
+  if (i < content.length) {
+    let txt = content.charAt(i);
+    text.innerHTML += txt === "`" ? "<br/>" : txt;
+    i++;
+  }
+}
+setInterval(typing, 500);
+
+text.addEventListener("click", function () {
+  let page = "https://seimin-bjj.github.io/PortFolio-R/";
   location.href = page;
 });
